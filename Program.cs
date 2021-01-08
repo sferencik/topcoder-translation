dbdc4b6c19e498603097fe1f1484524dae3da008
sferencik <sferencik@gmail.com>
2021-01-08 22:23:09 +0100

    Initial hello world

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace translation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
